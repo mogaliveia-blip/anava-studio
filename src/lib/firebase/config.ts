f4e0e2913370dbb7e0733a2d48c671e0e02070f6
Moga <eric.online6@gmail.com>
2026-05-05 11:31:36 +0200

    move firebase config to env variables

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD1V36wofU9zzs2FA52OzEtgxDvRZrpq7o",
-  authDomain: "anava-studio.firebaseapp.com",
-  projectId: "anava-studio",
-  storageBucket: "anava-studio.firebasestorage.app",
-  messagingSenderId: "671577461415",
-  appId: "1:671577461415:web:c26fce14e8928403be8182",
-  measurementId: "G-FCLH9GT96T"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
